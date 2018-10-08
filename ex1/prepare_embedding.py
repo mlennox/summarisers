f7a4a098793827a808b4ff59d389f37c148cb64c
Mark Lennox <mark@webpusher.ie>
2018-10-08 20:04:36 +0100

    we now can choose to load glove msgpack instead of raw files

--- ex1/prepare_embedding.py
+++ ex1/prepare_embedding.py
@@ -1,14 +1,17 @@
 from collections import Counter
 from itertools import chain
 from pandas import read_msgpack
-from numpy import array
+from numpy import array, empty, random
 from traceback import print_exc
-from msgpack import pack
-from msgpack_numpy import encode
+from msgpack import pack, unpack, packb, unpackb
+from os import path
+import msgpack_numpy as m
+
+m.patch()  # patch msgpack to use msgpack_numpy encoding/decoding
 
 
 def load_data(dataset_name):
-    print("Loading data")
+    print("Loading news dataset from msgpack")
     try:
         return read_msgpack("../datasets/%s.pack" % dataset_name)
     except Exception:
@@ -28,47 +31,102 @@ def build_vocabulary(word_list):
     return list(vocab), vocabcount
 
 
-def load_embeddings(embedding_dimension):
+def load_embeddings(embedding_dimension, force_load_glove):
     print("loading GloVe embeddings of dimension : %s" % embedding_dimension)
     try:
-        model = {}
-        with open(
-            "../datasets/glove.6B/glove.6B.%sd.txt" % embedding_dimension, "r"
-        ) as glove_file:
-            for line in glove_file:
-                splitLine = line.split()
-                word = splitLine[0]
-                embedding = array([float(val) for val in splitLine[1:]])
-                model[word] = embedding
-            save_embeddings(model)
-            return model
+        model_weights = {}
+        model_index = {}
+        model_exists = path.isfile("../datasets/glove.6B/model_weights.pack")
+        if model_exists and force_load_glove != True:
+            print("Loading existing msgpacked models")
+            model_weights = load_from_msgpack("model_weights")
+            model_index = load_from_msgpack("model_index")
+        else:
+            print("Loading raw GloVe embeddings and creating msgpack")
+            with open(
+                "../datasets/glove.6B/glove.6B.%sd.txt" % embedding_dimension, "r"
+            ) as glove_file:
+                index = 1
+                for line in glove_file:
+                    splitLine = line.split()
+                    word = splitLine[0]
+                    embedding = array([float(val) for val in splitLine[1:]])
+                    model_weights[index] = embedding
+                    model_index[word] = index
+                    index += 1
+                save_to_msgpack(model_weights, "model_weights")
+                save_to_msgpack(model_index, "model_index")
+        return model_weights, model_index
     except Exception:
         print("some issue loading the file or processing the weights")
         print_exc()
         exit()
 
 
-def save_embeddings(model):
-    print("Saving embedding model in msgpack format")
+def load_from_msgpack(filename):
+    print("Loading data '%s' from msgpack format" % filename)
     try:
-        with open("../datasets/glove.6B/model.pack", "wb") as outfile:
-            # x_enc = msgpack.packb(x, default=m.encode)
-            # x_rec = msgpack.unpackb(x_enc, object_hook=m.decode)
-            pack(model, outfile, default=encode)
+        with open("../datasets/glove.6B/%s.pack" % filename, "rb") as infile:
+            packed = infile.read()
+        return unpackb(packed)
     except Exception:
-        print("Could not save the embeddings model")
+        print("Could not load '%s'" % filename)
         print_exc()
         exit()
 
 
+def save_to_msgpack(data, filename):
+    print("Saving data '%s' in msgpack format" % filename)
+    try:
+        packed = packb(data)
+        with open("../datasets/glove.6B/%s.pack" % filename, "wb") as outfile:
+            outfile.write(packed)
+    except Exception:
+        print("Could not save '%s'" % filename)
+        print_exc()
+        exit()
+
+
+# copy any matching word embeddings from GloVe to our vocabulary matrix
+def create_vocab_matrix(
+    vocab_size, vocab, embedding_dimension, model_weights, model_index, glove_threshold
+):
+    print("Creating the vocabulary matrix")
+    # matrix_shape = (vocab_size, embedding_dimension)
+    # scale = 1  # what to do here...?
+    vocabulary_matrix = {}
+    # random.uniform(low= -scale, high=scale, size=matrix_shape)
+    model_index_keys = model_index.keys()
+
+    for index in range(vocab_size):
+        # check if the current word exists in the model
+        word = vocab[index]
+        if word in model_index_keys:
+            word_index = model_index[word]
+            vocabulary_matrix[word_index] = model_weights[word_index]
+    print("Entries in vocabulary matrix", len(vocabulary_matrix))
+
+    # now get nearest match embeddings for words that are not in the GloVe embeddings
+
+
 def run():
+    force_load_glove = False
     vocab_size = 40000
     embedding_dimension = 100
+    glove_threshold = 0.5
+    model_weights, model_index = load_embeddings(embedding_dimension, force_load_glove)
     df = load_data("combined_articles")
     vocab, vocabulary_count = build_vocabulary(
         df["title"].apply(remove_quotes) + df["content"].apply(remove_quotes)
     )
-    model = load_embeddings(embedding_dimension)
+    create_vocab_matrix(
+        vocab_size,
+        vocab,
+        embedding_dimension,
+        model_weights,
+        model_index,
+        glove_threshold,
+    )
 
 
 run()
