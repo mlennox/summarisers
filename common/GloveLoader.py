ac02d24010b27b73596701aa08758888473e299f
Mark Lennox <mark@webpusher.ie>
2018-10-23 09:10:23 +0100

    adding modules for loading and saving vectors and serialised models, creating new file to prepare embeddings matrix

--- common/GloveLoader.py
+++ common/GloveLoader.py
@@ -1,75 +1,62 @@
 import pandas as pd
 import numpy as np
 from json import loads
-import SerialiserPickle
+from common import SerialiserPickle
+from os import path
+from traceback import print_exc
 
 
 class GloveLoader:
     glovepath = "../datasets/glove.6B/"
 
-    def load(self, filename):
-        """
-        Load the Glove embeddings
-        
-        Returns:
-            [DataFrame] -- a pandas dataframe of the JSON data
-        """
+    def load(self, embedding_dimension, force_load_glove):
+        print("loading GloVe embeddings of dimension : %s" % embedding_dimension)
 
-        df = None
         try:
-            print("loading the JSONL")
-            json_list = []
-            with open(glovepath + filename, "r") as json_file:
-                for line in json_file:
-                    json_list.append(loads(line))
-            print("Converting to dataframe")
-            df = pd.io.json.json_normalize(json_list)
+            model_weights = {}
+            model_index = {}
+            model_exists = path.isfile(
+                "../datasets/glove.6B/model_weights.{0}.pkl".format(embedding_dimension)
+            )
+            if model_exists and force_load_glove != True:
+                print("Loading existing msgpacked models")
+                (model_weights, model_index) = self.deserialise(embedding_dimension)
+            else:
+                (model_weights, model_index) = self.load_vectors(embedding_dimension)
+                self.serialise(model_weights, embedding_dimension, "model_weights")
+                self.serialise(model_index, embedding_dimension, "model_index")
+            return model_weights, model_index
         except Exception as e:
-            print("Some problem loading the '{0}'".format(self.filename))
-            print(e)
+            print("some issue loading the file or processing the weights", e)
+            print_exc()
             exit()
 
-        print("Columns : ", df.columns)
-        print("Working on describing the data")
-        print(df.describe())
+    def load_vectors(self, embedding_dimension):
+        print("Loading raw GloVe embeddings")
+        model_weights = {}
+        model_index = {}
+        with open(
+            "{0}glove.6B.{1}.txt".format(self.glovepath, embedding_dimension), "r"
+        ) as glove_file:
+            index = 1
+            for line in glove_file:
+                splitLine = line.split()
+                word = splitLine[0]
+                embedding = [float(val) for val in splitLine[1:]]
+                model_weights[index] = embedding
+                model_index[word] = index
+                index += 1
+        return model_weights, model_index
 
-        return df
-
-    def serialise(self, model_weights, model_index):
+    def serialise(self, embedding_dimension, model_weights, model_index):
         """
         Serialise the loaded Glove model to speed up loading next time
         """
-        SerialiserPickle.save(model_weights, "model_weights")
-        SerialiserPickle.save(model_index, "model_index")
+        SerialiserPickle.save(model_weights, embedding_dimension, "model_weights")
+        SerialiserPickle.save(model_index, embedding_dimension, "model_index")
 
+    def deserialise(self. embedding_dimension):
+        model_weights = SerialiserPickle.load(embedding_dimension, "model_weights")
+        model_index = SerialiserPickle.load(embedding_dimension, "model_index")
+        return (model_weights, model_index)
 
-def load_embeddings(embedding_dimension, force_load_glove):
-    print("loading GloVe embeddings of dimension : %s" % embedding_dimension)
-    try:
-        model_weights = {}
-        model_index = {}
-        model_exists = path.isfile("../datasets/glove.6B/model_weights.pkl")
-        if model_exists and force_load_glove != True:
-            print("Loading existing msgpacked models")
-            model_weights = load_pickle("model_weights")
-            model_index = load_pickle("model_index")
-        else:
-            print("Loading raw GloVe embeddings and creating msgpack")
-            with open(
-                "../datasets/glove.6B/glove.6B.%sd.txt" % embedding_dimension, "r"
-            ) as glove_file:
-                index = 1
-                for line in glove_file:
-                    splitLine = line.split()
-                    word = splitLine[0]
-                    embedding = [float(val) for val in splitLine[1:]]
-                    model_weights[index] = embedding
-                    model_index[word] = index
-                    index += 1
-                save_pickle(model_weights, "model_weights")
-                save_pickle(model_index, "model_index")
-        return model_weights, model_index
-    except Exception:
-        print("some issue loading the file or processing the weights")
-        print_exc()
-        exit()
--- common/SerialiserPickle.py
+++ common/SerialiserPickle.py
@@ -3,24 +3,34 @@ from traceback import print_exc
 
 
 class SerialiserPickle:
-    def load(self, filename):
+    def load(self, embedding_dimension, filename):
         print("Loading data '%s' from pickle" % filename)
         try:
-            with open("../datasets/glove.6B/%s.pkl" % filename, "rb") as infile:
+            with open(
+                "../datasets/glove.6B/{0}.{1}.pkl".format(
+                    filename, embedding_dimension
+                ),
+                "rb",
+            ) as infile:
                 packed = pickle.load(infile)
             return packed
         except Exception:
-            print("Could not load '%s'" % filename)
+            print("Could not load '{0}.{1}'".format(filename, embedding_dimension))
             print_exc()
             exit()
 
-    def save(self, data, filename):
+    def save(self, data, embedding_dimension, filename):
         print("Loading data '%s' from pickle" % filename)
         try:
-            with open("../datasets/glove.6B/%s.pkl" % filename, "wb") as outfile:
+            with open(
+                "../datasets/glove.6B/{0}.{1}.pkl".format(
+                    filename, embedding_dimension
+                ),
+                "wb",
+            ) as outfile:
                 pickle.dump(data, outfile)
         except Exception:
-            print("Could not save '%s'" % filename)
+            print("Could not save '{0}.{1}'".format(filename, embedding_dimension))
             print_exc()
             exit()
 
